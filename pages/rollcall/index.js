5f2c6a9e728e9fecc28fb17a1d32cab0fd0e4c22
Huang Liang <exceedhl@gmail.com>
2017-01-16 18:46:52 +0800

    add some data

--- pages/rollcall/index.js
+++ pages/rollcall/index.js
@@ -3,6 +3,26 @@
 var app = getApp()
 Page({
   data: {
+    rollcalls: [
+      {'yearMonth': '2016年12月', 'days': [
+        {'day': '15日', 'weekDay': '周二', 'calls': [
+          {'name': '14:00的点名', 'class': '物流管理基础一班', 'status': 'done', 'peopleAttend': 28, 'peopleAll': 40},
+          {'name': '16:00的点名', 'class': '物流管理基础三班', 'status': 'ongoing'}
+        ]},
+        {'day': '14日', 'weekDay': '周一', 'calls': [
+          {'name': '14:00的点名', 'class': '物流管理基础一班', 'status': 'done', 'peopleAttend': 40, 'peopleAll': 40}
+        ]}
+      ]},
+      {'yearMonth': '2016年11月', 'days': [
+        {'day': '15日', 'weekDay': '周二', 'calls': [
+          {'name': '14:00的点名', 'class': '物流管理基础一班', 'status': 'done', 'peopleAttend': 22, 'peopleAll': 40},
+          {'name': '16:00的点名', 'class': '物流管理基础二班', 'status': 'todo'}
+        ]},
+        {'day': '12日', 'weekDay': '周一', 'calls': [
+          {'name': '14:00的点名', 'class': '物流管理基础一班', 'status': 'done', 'peopleAttend': 13, 'peopleAll': 40}
+        ]}
+      ]}
+    ]
   },
   onLoad: function () {
     console.log(this);
--- pages/rollcall/ongoing.js
+++ pages/rollcall/ongoing.js
@@ -4,6 +4,15 @@ var app = getApp()
 Page({
   data: {
   },
+
   onLoad: function () {
+  },
+
+  onShareAppMessage: function () {
+    return {
+      title: '点名邀请',
+      desc: '我发起了一个点名，各位同学请加入进来参与点名',
+      path: '/page/user?id=123'
+    }
   }
 })
